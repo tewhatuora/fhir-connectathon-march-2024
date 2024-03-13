e2972f8a7101e3a399b904b8e8297fe12ca13545
James Taylor <james.taylor@sparkhealth.nz>
2024-03-13 16:09:39 +1300

    Adding .NET Chnages

---
+++ NEMSGuaranteedSubscriber-dotNET/source/NEMSConfig.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.Logging;
+using SolaceSystems.Solclient.Messaging;
+
+namespace GuaranteedSubscriber;
+
+public class NEMSConfig
+    : IDisposable
+{
+    private readonly ILogger _logger;
+
+    public NEMSConfig(ILogger logger)
+    {
+        _logger = logger;
+
+        var properties = new ContextFactoryProperties
+        {
+            SolClientLogLevel = SolLogLevel.Warning,
+            LogDelegate = LogDelegate,
+        };
+        
+        ContextFactory.Instance.Init(properties);
+    }
+
+    public IContext CreateContext()
+    {
+        var context = ContextFactory.Instance.CreateContext(new ContextProperties(), null); 
+        return context;
+    }
+
+    public void Dispose()
+    {
+        ContextFactory.Instance.Cleanup();
+    }
+
+    private void LogDelegate(SolLogInfo logInfo)
+    {
+        _logger.Log(logInfo.LogLevel.ToExtensionsLogLevel(), logInfo.LogException, logInfo.LogMessage); ;
+    }
+}
---
+++ NEMSGuaranteedSubscriber-dotNET/source/Program.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using GuaranteedSubscriber;
+
+var configuration = new ConfigurationBuilder()
+    .AddJsonFile("properties.json")
+    .Build();
+
+var queueSection = configuration.GetSection("QueueConsumer");
+
+var settings = new QueueConsumerSettings
+{
+    Host = queueSection["Host"] ?? string.Empty,
+    VPNName = queueSection["VPNName"] ?? string.Empty,
+    Username = queueSection["Username"] ?? string.Empty,
+    Password = queueSection["Password"] ?? string.Empty,
+    QueueName = queueSection["QueueName"] ?? string.Empty,
+};
+
+#if (!DEBUG)
+try 
+{
+#endif 
+
+using var loggerFactory = LoggerFactory.Create(
+    builder => builder.AddConsole());
+
+
+using var consumer = new QueueConsumer(loggerFactory.CreateLogger<QueueConsumer>());
+
+using var config = new NEMSConfig(loggerFactory.CreateLogger<NEMSConfig>());
+using var context = config.CreateContext();
+consumer.Run(settings, context);
+
+#if (!DEBUG)
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex);
+}
+#endif 
